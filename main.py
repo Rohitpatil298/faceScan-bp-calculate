81998815ec4eaf187e0041de43897fa5216c3a6b
Rohit Anilbhai Patil <116003787+Rohitpatil298@users.noreply.github.com>
2026-02-03 16:11:00 +0530

    Set port to 8000 instead of using environment variable

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     uvicorn.run(
         app,
         host="0.0.0.0",
-        port=int(os.getenv("PORT", 8000)),
+        port=8000,
         reload=False,
         log_level="info",
     )
